b384622b38468977685fc39bfdf86d7caac9dd2c
Laurentg13 <laurent.gayraud@telecom-paris.fr>
2024-06-26 17:48:32 +0200

    final-implementation

--- visualisation_1_final.py
+++ visualisation_1_final.py
@@ -18,59 +18,66 @@ names['annais'] = names['annais'].astype(int)
 year_range = np.arange(1900, 2021, 1)
 
 # Select the range using a slider
-st.markdown("<b><small>Sélectionner une période</small></b>", unsafe_allow_html=True)
+st.markdown("<b><small>Sélectionner une période</small></b>",
+            unsafe_allow_html=True)
 start_year, end_year = st.select_slider(
     "Year Range Slider", options=year_range, value=(1900, 2020), label_visibility="collapsed"
 )
 
-filtered_names = names[(names['annais'] >= start_year) & (names['annais'] <= end_year)]
+filtered_names = names[(names['annais'] >= start_year)
+                       & (names['annais'] <= end_year)]
 
 # Initialize session state for selected names
 if 'selected_names' not in st.session_state:
     st.session_state.selected_names = []
 
 # Function to add a name
+
+
 def add_name():
     if name_to_add and name_to_add not in st.session_state.selected_names:
         st.session_state.selected_names.append(name_to_add)
 
 # Function to remove a name
+
+
 def remove_name():
     if name_to_remove in st.session_state.selected_names:
         st.session_state.selected_names.remove(name_to_remove)
 
+
 # Ensure filtered data is not empty
 if not filtered_names.empty:
     # Sum the counts per name for all regions and selected years
-    aggregated_names = filtered_names.groupby(['preusuel', 'sexe']).agg({'nombre': 'sum'}).reset_index()
+    aggregated_names = filtered_names.groupby(
+        ['preusuel', 'sexe']).agg({'nombre': 'sum'}).reset_index()
 
     # Map gender to labels
-    aggregated_names['gender'] = aggregated_names['sexe'].map({1: 'Male', 2: 'Female'})
+    aggregated_names['gender'] = aggregated_names['sexe'].map(
+        {1: 'Male', 2: 'Female'})
 
     # Filter top 20 male and top 20 female names
-    top_20_males = aggregated_names[aggregated_names['sexe'] == 1].nlargest(20, 'nombre')
-    top_20_females = aggregated_names[aggregated_names['sexe'] == 2].nlargest(20, 'nombre')
-
-    # Filter least 20 male and least 20 female names
-    least_20_males = aggregated_names[aggregated_names['sexe'] == 1].nsmallest(20, 'nombre')
-    least_20_females = aggregated_names[aggregated_names['sexe'] == 2].nsmallest(20, 'nombre')
+    top_20_males = aggregated_names[aggregated_names['sexe'] == 1].nlargest(
+        20, 'nombre')
+    top_20_females = aggregated_names[aggregated_names['sexe'] == 2].nlargest(
+        20, 'nombre')
 
     # Invert the order for male names and set y-axis on the right
     top_20_males['nombre'] = -top_20_males['nombre']
-    least_20_males['nombre'] = -least_20_males['nombre']
 
     # Combine the datasets for males and females
     top_combined_names = pd.concat([top_20_males, top_20_females])
-    least_combined_names = pd.concat([least_20_males, least_20_females])
 
     # Create the top names chart for males
     top_males_chart = alt.Chart(top_20_males).mark_bar().encode(
         x=alt.X('nombre:Q', title='', axis=alt.Axis(labels=True, format='~s')),
-        y=alt.Y('preusuel:N', title='', sort=None, axis=alt.Axis(orient='left', labels=False, ticks=False)),
+        y=alt.Y('preusuel:N', title='', sort=None, axis=alt.Axis(
+            orient='left', labels=False, ticks=False)),
         color=alt.value('blue'),
-        tooltip=[alt.Tooltip('preusuel:N', title='prenom'), alt.Tooltip('nombre:Q', title='nombre'), alt.Tooltip('gender:N', title='sexe')]
+        tooltip=[alt.Tooltip('preusuel:N', title='prenom'), alt.Tooltip(
+            'nombre:Q', title='nombre'), alt.Tooltip('gender:N', title='sexe')]
     ).properties(
-        width=300,
+        width=400,
         height=400
     )
 
@@ -95,11 +102,13 @@ if not filtered_names.empty:
     # Create the top names chart for females
     top_females_chart = alt.Chart(top_20_females).mark_bar().encode(
         x=alt.X('nombre:Q', title='', axis=alt.Axis(labels=True, format='~s')),
-        y=alt.Y('preusuel:N', title='', sort=None, axis=alt.Axis(orient='right', labels=False, ticks=False)),
+        y=alt.Y('preusuel:N', title='', sort=None, axis=alt.Axis(
+            orient='right', labels=False, ticks=False)),
         color=alt.value('pink'),
-        tooltip=[alt.Tooltip('preusuel:N', title='prenom'), alt.Tooltip('nombre:Q', title='nombre'), alt.Tooltip('gender:N', title='sexe')]
+        tooltip=[alt.Tooltip('preusuel:N', title='prenom'), alt.Tooltip(
+            'nombre:Q', title='nombre'), alt.Tooltip('gender:N', title='sexe')]
     ).properties(
-        width=300,
+        width=400,
         height=400
     )
 
@@ -133,84 +142,13 @@ if not filtered_names.empty:
         titleFontSize=14
     )
 
-    # Create the least names chart for males
-    least_males_chart = alt.Chart(least_20_males).mark_bar().encode(
-        x=alt.X('nombre:Q', title='', axis=alt.Axis(labels=True, format='~s')),
-        y=alt.Y('preusuel:N', title='', sort=None, axis=alt.Axis(orient='left', labels=False, ticks=False)),
-        color=alt.value('blue'),
-        tooltip=[alt.Tooltip('preusuel:N', title='prenom'), alt.Tooltip('nombre:Q', title='nombre'), alt.Tooltip('gender:N', title='sexe')]
-    ).properties(
-        width=300,
-        height=400
-    )
-
-    least_males_text = least_males_chart.mark_text(
-        align='right',
-        baseline='middle',
-        dx=-3
-    ).encode(
-        text='preusuel:N',
-        color=alt.value('white')
-    )
-
-    least_males_numbers = least_males_chart.mark_text(
-        align='left',
-        baseline='middle',
-        dx=3,
-        color='blue'
-    ).encode(
-        text='nombre:Q'
-    )
-
-    # Create the least names chart for females
-    least_females_chart = alt.Chart(least_20_females).mark_bar().encode(
-        x=alt.X('nombre:Q', title='', axis=alt.Axis(labels=True, format='~s')),
-        y=alt.Y('preusuel:N', title='', sort=None, axis=alt.Axis(orient='right', labels=False, ticks=False)),
-        color=alt.value('pink'),
-        tooltip=[alt.Tooltip('preusuel:N', title='prenom'), alt.Tooltip('nombre:Q', title='nombre'), alt.Tooltip('gender:N', title='sexe')]
-    ).properties(
-        width=300,
-        height=400
-    )
-
-    least_females_text = least_females_chart.mark_text(
-        align='left',
-        baseline='middle',
-        dx=3
-    ).encode(
-        text='preusuel:N',
-        color=alt.value('white')
-    )
-
-    least_females_numbers = least_females_chart.mark_text(
-        align='right',
-        baseline='middle',
-        dx=-3,
-        color='pink'
-    ).encode(
-        text='nombre:Q'
-    )
-
-    least_combined_chart = alt.hconcat(
-        (least_males_chart + least_males_text + least_males_numbers),
-        (least_females_chart + least_females_text + least_females_numbers)
-    ).resolve_scale(
-        x='independent'
-    ).properties(
-        title='Top 20 des prénoms masculins et féminins les moins populaires'
-    ).configure_axis(
-        labelFontSize=12,
-        titleFontSize=14
-    )
-
-    # Display the top and least combined charts
+    # Display the top combined chart
     st.altair_chart(top_combined_chart.configure_title(
-        fontSize=24, anchor='start'), use_container_width=True)
-    st.altair_chart(least_combined_chart.configure_title(
         fontSize=24, anchor='start'), use_container_width=True)
 
     # Create columns for adding and removing names
-    st.markdown("<b><small>Ajouter ou retirer des prénoms pour la courbe de popularité</small></b>", unsafe_allow_html=True)
+    st.markdown("<b><small>Ajouter ou retirer des prénoms pour la courbe de popularité</small></b>",
+                unsafe_allow_html=True)
     col1, col2 = st.columns(2)
 
     with col1:
@@ -234,10 +172,12 @@ if not filtered_names.empty:
         # Calculate the rank for each year
         yearly_counts = filtered_names.groupby(['annais', 'preusuel']).agg({
             'nombre': 'sum'}).reset_index()
-        yearly_counts['rank'] = yearly_counts.groupby('annais')['nombre'].rank(method='first', ascending=False)
+        yearly_counts['rank'] = yearly_counts.groupby(
+            'annais')['nombre'].rank(method='first', ascending=False)
 
         # Get the rank for the selected names
-        name_rank_data = yearly_counts[yearly_counts['preusuel'].isin(st.session_state.selected_names)]
+        name_rank_data = yearly_counts[yearly_counts['preusuel'].isin(
+            st.session_state.selected_names)]
 
         # Create a selection that allows zooming
         zoom = alt.selection_interval(bind='scales')
@@ -245,13 +185,15 @@ if not filtered_names.empty:
         # Create the popularity over time chart
         popularity_chart = alt.Chart(name_rank_data).mark_line(point=alt.OverlayMarkDef(color='orange'), color='orange').encode(
             x=alt.X('annais:O', title='Année', axis=alt.Axis(format='d')),
-            y=alt.Y('rank:Q', title='Rang', scale=alt.Scale(domain=(0, name_rank_data['rank'].max() + 1))),
+            y=alt.Y('rank:Q', title='Rang', scale=alt.Scale(
+                domain=(0, name_rank_data['rank'].max() + 1))),
             color='preusuel:N',
-            tooltip=[alt.Tooltip('annais:O', title='annee'), alt.Tooltip('rank:Q', title='rang')]
+            tooltip=[alt.Tooltip('annais:O', title='annee'),
+                     alt.Tooltip('rank:Q', title='rang')]
         ).properties(
             title="Popularité des prénoms au cours des années",
-            width=600,
-            height=400
+            width=1000,
+            height=600
         ).add_selection(zoom)
 
         popularity_text = popularity_chart.mark_text(
@@ -269,6 +211,6 @@ if not filtered_names.empty:
 
         # Display the popularity chart
         st.altair_chart(popularity_combined_chart.configure_title(
-            fontSize=24, anchor='start'), use_container_width=True)
+            fontSize=24, anchor='start'), use_container_width=False)
 else:
     st.write("No data available for the selected year range.")
